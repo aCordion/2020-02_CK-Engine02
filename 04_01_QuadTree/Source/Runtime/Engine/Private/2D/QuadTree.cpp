2bf10cb7992e6406a86301c472367865d1d48a74
aCordion <adlftn0209@naver.com>
2020-10-06 23:43:41 +0900

    QuadTree
    incomplete

---
+++ 04_01_QuadTree/Source/Runtime/Engine/Private/2D/QuadTree.cpp
@@ -0,0 +1,123 @@
+#include "Precompiled.h"
+
+// 핵심 로직 
+bool QuadTree::Insert(const std::string& InKey, const Rectangle& InBound)
+{
+
+	// 마지막 Depth까지 도달하면 자료구조에 추가하고 종료 { return true; }
+	if (Level >= MaxLevel)
+	{
+		Nodes.push_back(TreeNode(InKey, InBound));
+		return true;
+	}
+	else
+	{
+		if (IsLeaf || SubTrees == nullptr)
+		{
+			Split();
+		}
+
+		QuadTree* qt = FindSubTree(InBound);
+		if (qt == nullptr)
+		{
+			Nodes.push_back(TreeNode(InKey, InBound));
+			return true;
+		}
+		else
+		{
+			qt->Insert(InKey, InBound);
+			return false;
+			
+			/*if (qt->Bound.Intersect(InBound))
+			{
+				
+			}*/
+		}
+
+		return false;
+	}
+	// 자식 노드를 만들어야 하는 경우 4개의 자식 노드를 새롭게 생성 => Split()
+	// Split된 상태에서 FindSubTree 함수의 결과에 따라 처리를 다르게 진행
+	// 1. 겹쳐 있으면 자신의 자료구조에 넣고 끝내기 ( return true; )
+	// 2. 안겹쳐져 있으면 자식으로 내려보내기 ( childNode->Insert ( 재귀 호출 ) )
+
+}
+
+void QuadTree::Split()
+{
+	// SW, SE, NW, SE 네 개의 영역으로 쪼개고 이들의 바운딩 볼륨을 계산
+
+	//Vector2 center = Bound.Max * 0.5f;
+	//Vector2 center = Bound.Max - Bound.Min;
+	Vector2 center = Vector2::Zero;
+
+	//if (   (Bound.Max - center).X < 0.0f
+	//	&& (Bound.Max - center).Y < 0.0f)
+	//{
+	//	// 나눌 수 있는지 확실치 않음
+	//	return;
+	//}
+
+	SubTrees[SubNames::TopLeft] = new QuadTree(Rectangle(Vector2(Bound.Min.X, center.Y), Vector2(center.X, Bound.Max.Y)), Level + 1);
+	SubTrees[SubNames::TopRight] = new QuadTree(Rectangle(center, Bound.Max), Level + 1);
+	SubTrees[SubNames::BottomLeft] = new QuadTree(Rectangle(Bound.Min, center), Level + 1);
+	SubTrees[SubNames::BottomRight] = new QuadTree(Rectangle(Vector2(center.X, Bound.Min.Y), Vector2(Bound.Max.X, center.Y)), Level + 1);
+	IsLeaf = false;
+}
+
+QuadTree* QuadTree::FindSubTree(const Rectangle& InBound)
+{
+	if (IsLeaf)
+	{
+		return nullptr;
+	}
+
+	// 네 개의 자식 노드를 돌면서 완전 포함하는 자식 트리가 있는지 조사.
+	/*for (QuadTree* subTree : SubTrees)
+	{
+		assert(subTree != nullptr);
+		if (subTree->Contains(InBound))
+		{
+			return subTree;
+		}
+	}*/
+	for (int i = 0; i < 4; i++)
+	{
+		if (SubTrees == nullptr)
+		{
+			return nullptr;
+		}
+		if (SubTrees[i]->Contains(InBound))
+		{
+			return SubTrees[i];
+		}
+	}
+
+
+	return nullptr;
+}
+
+void QuadTree::Query(const CK::Rectangle& InRectangleToQuery, std::vector<std::string>& InOutItems) const
+{
+
+}
+
+void QuadTree::Clear()
+{
+
+	// 서브 트리가 없을 경우
+	if (SubTrees == nullptr)
+	{
+		return;
+	}
+	else
+	{
+		//delete SubTrees;
+	}
+
+}
+
+bool QuadTree::Contains(const Rectangle& InBox) const
+{
+	return InBox.IsInside(Bound);
+}
